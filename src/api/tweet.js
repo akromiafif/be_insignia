75b93f426da2f800928e15b737337949fdc79ce2
Afif Akromi <massachusetts48@gmail.com>
2022-12-02 22:48:52 +0700

    add transaction

--- src/api/tweet.js
+++ src/api/tweet.js
@@ -1,55 +1,75 @@
 const express = require("express");
+const sequelize = require("sequelize");
 const Tweet = require("../models/tweet");
+const t = await sequelize.transaction();
 
 const router = express.Router();
 
 router.get("/tweet", async (req, res) => {
-  const tweetUser = await Tweet.findAll().catch((err) => {
-    console.log("Error: ", err);
-  });
-
-  if (tweetUser)
-    return res.status(400).json({
-      message: "Get tweet information",
-      result: tweetUser,
+  try {
+    const tweetUser = await Tweet.findAll({ transaction: t }).catch((err) => {
+      console.log("Error: ", err);
     });
 
-  res.json({
-    message: "Tweet not found",
-  });
+    if (tweetUser)
+      return res.status(400).json({
+        message: "Get tweet information",
+        result: tweetUser,
+      });
+
+    await t.commit();
+
+    res.json({
+      message: "Tweet not found",
+    });
+  } catch (err) {
+    await t.rollback();
+  }
 });
 
 router.post("/tweet", async (req, res) => {
   const { userId, like, content } = req.body;
 
-  const newTweet = new Tweet({
-    userId,
-    like,
-    content,
-  });
-  const savedUser = await newTweet.save().catch((err) => {
-    console.log("Error: ", err);
-    res.status(500).json({ error: "Cannot tweeting at the moment!" });
-  });
+  try {
+    const newTweet = new Tweet({
+      userId,
+      like,
+      content,
+    });
+    const savedUser = await newTweet.save().catch((err) => {
+      console.log("Error: ", err);
+      res.status(500).json({ error: "Cannot tweeting at the moment!" });
+    });
 
-  if (savedUser) res.json({ message: "Thanks for tweeting" });
+    if (savedUser) res.json({ message: "Thanks for tweeting" });
+
+    await t, commit();
+  } catch (err) {
+    await t.rollback();
+  }
 });
 
 router.delete("/tweet/:id", async (req, res) => {
   const id = req.params.id;
 
-  const delTweetWithId = await Tweet.destroy({
-    where: { id },
-  }).catch((err) => {
-    console.log("Error: ", err);
-  });
+  try {
+    const delTweetWithId = await Tweet.destroy({
+      where: { id },
+    }).catch((err) => {
+      console.log("Error: ", err);
+    });
 
-  if (delTweetWithId)
-    return res.status(400).json({ message: "Tweet deleted successfully" });
+    if (delTweetWithId)
+      return res.status(400).json({ message: "Tweet deleted successfully" });
 
-  res.json({
-    message: "Tweet not found",
-  });
+    res.json({
+      message: "Tweet not found",
+    });
+
+    await t, commit();
+  } catch (err) {
+    await t.rollback();
+  }
 });
 
 router.put("/tweet/:id", async (req, res) => {
@@ -59,49 +79,60 @@ router.put("/tweet/:id", async (req, res) => {
   const like = req.body.like;
   const content = req.body.content;
 
-  const updateTweetById = await Tweet.upsert({
-    id,
-    userId,
-    like,
-    content,
-  }).catch((err) => {
-    console.log("Error: ", err);
-  });
-
-  if (updateTweetById)
-    return res.status(200).json({
-      message: "Update tweet successfully",
+  try {
+    const updateTweetById = await Tweet.upsert({
+      id,
+      userId,
+      like,
+      content,
+    }).catch((err) => {
+      console.log("Error: ", err);
     });
 
-  res.json({
-    message: "Tweet not found",
-  });
+    if (updateTweetById)
+      return res.status(200).json({
+        message: "Update tweet successfully",
+      });
+
+    res.json({
+      message: "Tweet not found",
+    });
+
+    await t, commit();
+  } catch (err) {
+    await t.rollback();
+  }
 });
 
 router.patch("/tweet/:id", async (req, res) => {
   const id = req.params.id;
-
   const content = req.body.content;
 
-  const updateTweetById = await Tweet.update(
-    {
-      content,
-    },
-    {
-      where: { id },
-    }
-  ).catch((err) => {
-    console.log("Error: ", err);
-  });
-
-  if (updateTweetById)
-    return res.status(200).json({
-      message: "Update tweet successfully",
+  try {
+    const updateTweetById = await Tweet.update(
+      {
+        content,
+      },
+      {
+        where: { id },
+      }
+    ).catch((err) => {
+      console.log("Error: ", err);
     });
 
-  res.json({
-    message: "Tweet not found",
-  });
+    if (updateTweetById)
+      return res.status(200).json({
+        message: "Update tweet successfully",
+      });
+
+    res.json({
+      message: "Tweet not found",
+    });
+
+    await t, commit();
+  } catch (err) {
+    await t.rollback();
+  }
 });
 
 module.exports = router;
