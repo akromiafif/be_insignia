faaa0f783592c04bcacad4d60b771a48798ced4b
Afif Akromi <massachusetts48@gmail.com>
2022-12-03 02:38:18 +0700

    add post create user testing

--- test/api.test.js
+++ test/api.test.js
@@ -27,3 +27,20 @@ describe("GET /api/v1/tweet", () => {
       .expect(200, done);
   });
 });
+
+describe("POST /api/v1/register", () => {
+  it("responds with user", (done) => {
+    request(app)
+      .post("/api/v1/register")
+      .send({
+        fullName: "Test Akromi",
+        username: "test",
+        phoneNumber: "0931235234",
+        email: "test@gmail.com",
+        password: "test123",
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200, done);
+  });
+});
