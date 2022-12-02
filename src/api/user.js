b6d76346e17a3bf61b31a3aeefd524f0cbbc7276
Afif Akromi <massachusetts48@gmail.com>
2022-12-02 20:13:10 +0700

    add update user

--- src/api/user.js
+++ src/api/user.js
@@ -8,12 +8,6 @@ router.delete("/user", async (req, res) => {
   const jwtToken = req.body.access_token;
   const result = jwt.verify(jwtToken, process.env.JWT_SECRET);
 
-  const userWithEmail = await User.findOne({
-    where: { email: result.email },
-  }).catch((err) => {
-    console.log("Error: ", err);
-  });
-
   const delUserWithEmail = await User.destroy({
     where: { email: result.email },
   }).catch((err) => {
@@ -22,6 +16,41 @@ router.delete("/user", async (req, res) => {
 
   if (delUserWithEmail)
     return res.status(400).json({ message: "User deleted successfully" });
+
+  res.json({
+    message: "User not found",
+  });
+});
+
+router.put("/user", async (req, res) => {
+  const jwtToken = req.body.access_token;
+  const result = jwt.verify(jwtToken, process.env.JWT_SECRET);
+
+  const fullName = req.body.fullName;
+  const username = req.body.username;
+  const phoneNumber = req.body.phoneNumber;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const updateUserWithEmail = await User.update(
+    {
+      fullName,
+      username,
+      phoneNumber,
+      email,
+      password,
+    },
+    {
+      where: { email: result.email },
+    }
+  ).catch((err) => {
+    console.log("Error: ", err);
+  });
+
+  if (updateUserWithEmail)
+    return res.status(400).json({
+      message: "User updated successfully",
+    });
 
   res.json({
     message: "User not found",
