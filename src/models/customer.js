8090186da094d26510119daf51fa8d36abe90692
Afif Akromi <massachusetts48@gmail.com>
2022-12-04 14:58:26 +0700

    fix get user

--- src/models/customer.js
+++ src/models/customer.js
@@ -1,35 +1,46 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../database");
 const Order = require("./order");
+const redisClient = require("../database/redis");
 
-const Customer = sequelize.define("Customer", {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    allowNull: false,
-    primaryKey: true,
+const Customer = sequelize.define(
+  "Customer",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  address: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  phoneNumber: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  {
+    hooks: {
+      afterSave: (user, options) => {
+        redisClient.del("users");
+      },
+    },
+  }
+);
 
 Customer.hasMany(Order, { foreignKey: "customerId" });
 module.exports = Customer;
