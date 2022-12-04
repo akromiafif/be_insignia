b73beaf386ddaccc72760f9ec5d59ea38277bcd8
Afif Akromi <massachusetts48@gmail.com>
2022-12-04 15:35:49 +0700

    get all order n order by id

--- src/api/order.js
+++ src/api/order.js
@@ -20,4 +20,40 @@ router.post("/order", async (req, res) => {
   if (savedPackage) res.json({ message: "Order created successfully" });
 });
 
+router.get("/order", async (req, res) => {
+  const allOrder = await Order.findAll().catch((err) => {
+    console.log("Error: ", err);
+  });
+
+  if (allOrder)
+    return res.status(200).json({
+      message: "Get all orders information",
+      result: allOrder,
+    });
+
+  res.json({
+    message: "Orders not found",
+  });
+});
+
+router.get("/order/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const orderById = await Order.findOne({
+    where: { id },
+  }).catch((err) => {
+    console.log("Error: ", err);
+  });
+
+  if (orderById)
+    return res.status(200).json({
+      message: "Get order information",
+      result: orderById,
+    });
+
+  res.json({
+    message: "Order not found",
+  });
+});
+
 module.exports = router;
